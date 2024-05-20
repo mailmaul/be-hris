20afc2060ceef9acb37b1532a1f78739fdee03be
Maul <62276420+mailmaul@users.noreply.github.com>
2024-05-20 14:29:10 +0700

    add login-signup API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const pasth = require("path");
+const path = require("path");
 const bcrypt = require("bcrypt");
 const collection = require("./config");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static("public"));
 
 app.use(express.urlencoded({extended: false}));
 
@@ -20,16 +22,49 @@ app.get("/signup", (req, res) => {
 });
 
 app.post("/signup", async (req, res) => {
+
     const data = {
         name: req.body.username,
         password: req.body.password
     }
 
-    const userdata = await collection.insertMany(data);
-    console.log(userdata);
-})
+    // Check if the username already exists in the database
+    const existingUser = await collection.findOne({ name: data.name });
 
+    if (existingUser) {
+        res.send('User already exists. Please choose a different username.');
+    } else {
+        // Hash the password using bcrypt
+        const saltRounds = 10; // Number of salt rounds for bcrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
+        data.password = hashedPassword; // Replace the original password with the hashed one
+
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.send("User name cannot found")
+        }
+        // Compare the hashed password from the database with the plaintext password
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!isPasswordMatch) {
+            res.send("wrong Password");
+        }
+        else {
+            res.render("home");
+        }
+    }
+    catch {
+        res.send("wrong Details");
+    }
+});
 
 const port = 5000;
 app.listen(port, () => {
